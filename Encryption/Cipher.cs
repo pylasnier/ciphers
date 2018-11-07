3f4ebc74e8007e588a23d4c77b2447e0301b00e8
Pascal Lasnier <py@lasnier.com>
2018-11-07 15:31:18 +0000

    College: Messing around mostly

--- Encryption/Cipher.cs
+++ Encryption/Cipher.cs
@@ -1,9 +1,30 @@
-﻿namespace Encryption
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
 {
+    //Used to convey information about key type and use
+    public class SubKey
+    {
+        public Type KeyType { get; set; }
+        public string Name { get; set; }
+
+        public SubKey(Type keyType, string name)
+        {
+            KeyType = keyType;
+            Name = name;
+        }
+    }
+
     //Interface used to write cipher classes
     public interface ICipher<T>
     {
-        char[] Encrypt(char[] plainText, T key);
+        List<SubKey> KeyStructure { get; }
+
+        char[] Encrypt(IEnumerable<object> vals);
         char[] Encrypt(string plainText, T key);
         char[] Decrypt(char[] cipherText, T key);
         char[] Decrypt(string cipherText, T key);
